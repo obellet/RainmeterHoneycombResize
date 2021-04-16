1dfb155d8c357107d9d698d9e56cd6970dd85f21
obellet <octave.bellet1997@gmail.com>
2021-04-16 12:44:37 -0400

    comments for clarity

--- code_v1.py
+++ code_v1.py
@@ -10,7 +10,7 @@ def set_value_in_property_file(file_path, section, key, value):
     config.write(cfgfile, space_around_delimiters=False)  # use flag in case case you need to avoid white space.
     cfgfile.close()
 
-directory = r"C:\Users\Octave\Documents\Rainmeter\Skins\Honeycomb"
+directory = r"C:\Users\Octave\Documents\Rainmeter\Skins\Honeycomb" # use your directory containing the honeycomb .ini files
 
 for subdir, dirs, files in os.walk(directory):
     for filename in files:
@@ -20,6 +20,6 @@ for subdir, dirs, files in os.walk(directory):
             config = ConfigParser()
             config.read(filepath)
             section_list = list(config.sections())
-            set_value_in_property_file(filepath, section_list[0], 'H', '110') # hello this is tavy
+            set_value_in_property_file(filepath, section_list[0], 'H', '110') # change the value 110 to adjust the size of the icons
         else:
             continue
