7574dfdf16adf4abb8da0a526a5a235ed69d0e3d
obellet <octave.bellet1997@gmail.com>
2021-04-16 12:26:49 -0400

    hello

---
+++ code_v1.py
@@ -0,0 +1,25 @@
+import os
+import configparser
+from configparser import ConfigParser
+
+def set_value_in_property_file(file_path, section, key, value):
+    config = configparser.RawConfigParser()
+    config.read(file_path)
+    config.set(section,key,value)                         
+    cfgfile = open(file_path,'w')
+    config.write(cfgfile, space_around_delimiters=False)  # use flag in case case you need to avoid white space.
+    cfgfile.close()
+
+directory = r"C:\Users\Octave\Documents\Rainmeter\Skins\Honeycomb"
+
+for subdir, dirs, files in os.walk(directory):
+    for filename in files:
+        filepath = subdir + os.sep + filename
+
+        if filepath.endswith(".ini"):
+            config = ConfigParser()
+            config.read(filepath)
+            section_list = list(config.sections())
+            set_value_in_property_file(filepath, section_list[0], 'H', '110') # hello
+        else:
+            continue
